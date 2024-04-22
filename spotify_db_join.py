2beedb68107a4ee1d4107a526fa640101f9bc1b4
Kaiping Lu <kaipingl@umich.edu>
2024-04-22 13:42:06 -0400

    psuh

--- spotify_db_join.py
+++ spotify_db_join.py
@@ -15,13 +15,13 @@ def setup_database():
     );
     ''')
     cursor.execute('''
-    CREATE TABLE IF NOT EXISTS insert_tracker (
+    CREATE TABLE IF NOT EXISTS playlist_insert_tracker (
         id INTEGER PRIMARY KEY,
         last_inserted_index INTEGER DEFAULT 0
     );
     ''')
     # Ensure there is a row to track the position
-    cursor.execute('INSERT OR IGNORE INTO insert_tracker (id, last_inserted_index) VALUES (1, 0);')
+    cursor.execute('INSERT OR IGNORE INTO playlist_insert_tracker (id, last_inserted_index) VALUES (1, 0);')
     
     conn.commit()
     cursor.close()
@@ -32,7 +32,7 @@ def execute_join_and_store_results():
     cursor = conn.cursor()
 
     # Retrieve the last inserted index
-    cursor.execute('SELECT last_inserted_index FROM insert_tracker WHERE id = 1;')
+    cursor.execute('SELECT last_inserted_index FROM playlist_insert_tracker WHERE id = 1;')
     last_index = cursor.fetchone()[0]
 
     query = '''
@@ -48,10 +48,12 @@ def execute_join_and_store_results():
     cursor.execute(query)
     join_results = cursor.fetchall()
 
+    # Determine the next batch based on the last inserted index
     start_index = last_index
     end_index = start_index + 25
     batch = join_results[start_index:end_index]
 
+    # Insert the batch into the playlist_tracks table
     cursor.executemany('''
     INSERT INTO playlist_tracks (playlist_name, track_name, artist)
     VALUES (?, ?, ?)
@@ -60,7 +62,7 @@ def execute_join_and_store_results():
 
     # Update the last inserted index in insert_tracker
     new_last_index = min(end_index, len(join_results))
-    cursor.execute('UPDATE insert_tracker SET last_inserted_index = ? WHERE id = 1;', (new_last_index,))
+    cursor.execute('UPDATE playlist_insert_tracker SET last_inserted_index = ? WHERE id = 1;', (new_last_index,))
 
     conn.commit()
     cursor.close()
